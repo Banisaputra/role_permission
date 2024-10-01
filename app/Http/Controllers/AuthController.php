b74193b32f8bfcd20b89e3c82de02f513a47d5e0
Banisaputra <bani.s26shafa@gmail.com>
2024-10-01 16:21:59 +0700

    add: auth theme

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        $request->validate([
+            'name' =>'required|string|max:100',
+            'email' =>'required|string|email|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        return response()->json(['token' => $token]);
+    }
+    public function login(Request $request) {
+        $credentials = $request->only('email', 'password');
+        $remember = !empty($request->rememberMe) ? true : false;
+
+        if(Auth::attempt($credentials, $remember)) {
+            return redirect('panel/dashboard');
+        } else {
+            return redirect()->back()->with('error', 'Please enter current email or password');
+        }
+    }
+
+    public function loginForm() {
+        return view('auth.login');
+    }
+
+    public function registerForm() {
+        return view('auth.register');
+    }
+
+    public function forgotPasswordForm() {
+        return view('auth.forgot-password');
+    }
+
+    public function logout() {
+        Auth::logout();
+
+        return redirect()->route('login');
+    
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/register', [AuthController::class,'registerForm'])->name('register');
+Route::post('/register', [AuthController::class,'register']);
+Route::get('/login', [AuthController::class, 'loginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/forgotPassword', [AuthController::class, 'forgotPasswordForm'])->name('forgotPassword');
+
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+
+Route::get('panel/dashboard', [DashboardController::class, 'index']);
